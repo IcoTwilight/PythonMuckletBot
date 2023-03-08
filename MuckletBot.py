991644c020d958b28999a2dc71bc02f2e5fda92c
IcoTwilight <95135986+IcoTwilight@users.noreply.github.com>
2023-03-08 11:10:44 +0000

    Updated
    added various fixes and stability improvements while re-writing the message class and adding a getId and quit method.

--- MuckletBot.py
+++ MuckletBot.py
@@ -26,8 +26,22 @@ ADDRESS = 15
 MESSAGE = 16
 PING = 17
 
-class Player:
-    def __init__(self, bot, playerFirstName, playerSurName, playerID):
+class Logger:
+    def __init__(self, type = "Log", wraping = "=", indent = 4, indentChar = " "):
+        self.type = type
+        self.wraping = wraping
+        self.indent = indent
+        self.indentChar = indentChar
+        self.i = 1
+    def log(self, *messages):
+        print(f"{self.wraping*self.indent} {self.type} {self.wraping*self.indent}")
+        for message in messages:
+            print(f"{self.i}: {self.indentChar*self.indent}{message}")
+            self.i += 1
+        print(f"{self.wraping*(self.indent*2+len(self.type)+2)}")
+
+class Character:
+    def __init__(self, bot, playerFirstName = None, playerSurName = None, playerID = None):
         self.bot = bot
         self.firstName = playerFirstName
         self.surName = playerSurName
@@ -39,39 +53,100 @@ class Player:
 class Message:
     def __init__(self, bot, message):
         self.bot = bot
-        self.message = message
-        # if the message ends with ctrl.out then it is a message composed by a player.
-        self.event = self.message.get("event")
-        if not self.event:
-            self.loggedMessage = False
+        self.raw = message
+        if self.raw.get("error"):
+            self.errorCode = self.raw.get("error").get("code")
+            self.errorMessage = self.raw.get("error").get("message")
             return
-        event = self.event.split(".")
-        event = event[-2:]
-        if event == ["ctrl", "out"]:
-            self.loggedMessage = True
         else:
-            self.loggedMessage = False
-        # get the message's data.
-        self.data = self.message.get("data")
-        if not self.data:
-            return
-        try:
-            self.id = self.data.get("id")
-            self.type = self.data.get("type")
-            self.text = self.data.get("msg")
-            self.time = self.data.get("time")
-            self.sig  = self.data.get("sig")
-            self.sender = self.data.get("char")
-            if self.sender:
-                self.sender = Player(self.bot, self.sender.get("name"), self.sender.get("surname"), self.sender.get("id"))
-                self.isme = self.sender.id == self.bot.CID
-            self.target = self.data.get("target")
-            if self.target:
-                self.target = Player(self.bot, self.target.get("name"), self.target.get("surname"), self.target.get("id"))
-        except:
-            return
+            self.errorCode = None
+            self.errorMessage = None
+        if self.result():
+            self.eventPlace = None
+            self.eventCID = None
+            self.eventType = None
+        else:
+            split = self.raw.get("event").split(".") # split the event into its parts.
+            self.eventPlace = split[1]
+            self.eventCID = split[2]
+            self.eventType = ".".join(split[3:])
+    def type(self):
+        if self.raw.get("data"):
+            return self.raw.get("data").get("type")
+        return None
+    def sender(self):
+        if self.raw.get("data"):
+            char = self.raw.get("data").get("char")
+            if char:
+                # cunstruct a new character object.
+                return Character(self.bot, char.get("name"), char.get("surname"), char.get("id"))
+        return None
+    def message(self):
+        if self.raw.get("data"):
+            return self.raw.get("data").get("msg")
+        return None
+    def time(self):
+        if self.raw.get("data"):
+            return self.raw.get("data").get("time")
+        return None
+    def signature(self):
+        if self.raw.get("data"):
+            return self.raw.get("data").get("sig")
+        return None
+    def id(self):
+        if self.raw.get("data"):
+            return self.raw.get("data").get("id")
+        return None
+    def target(self):
+        if self.raw.get("data"):
+            target = self.raw.get("data").get("target")
+            if target:
+                # cunstruct a new character object.
+                return Character(self.bot, target.get("name"), target.get("surname"), target.get("id"))
+        return None
+    def event(self):
+        return self.raw.get("event")
+    def fromSelf(self):
+        if self.sender():
+            return self.sender().id == self.bot.CID
+        else:
+            return None
+    def toSelf(self):
+        return self.eventCID == self.bot.CID
+    def result(self):
+        return self.raw.get("result")
+    
+    # fromself returns true if the message is from the bot and not another awake character.
+    # toSelf returns true if the message is for the bot and not another awake character.
+
+    def __str__(self):
+        string = f"Message Object:\n"
+        string += f"\t{self.raw=}\n"
+        string += f"\t{self.eventPlace=}\n"
+        string += f"\t{self.eventCID=}\n"
+        string += f"\t{self.eventType=}\n"
+        string += f"\t{self.type()=}\n"
+        string += f"\t{self.sender()=}\n"
+        string += f"\t{self.message()=}\n"
+        string += f"\t{self.time()=}\n"
+        string += f"\t{self.signature()=}\n"
+        string += f"\t{self.id()=}\n"
+        string += f"\t{self.target()=}\n"
+        string += f"\t{self.event()=}\n"
+        string += f"\t{self.fromSelf()=}\n"
+        string += f"\t{self.toSelf()=}\n"
+        string += f"\t{self.result()=}\n"
+        return string
+    
+
 class Bot:
     def __init__(self, TOKEN = None, USERNAME = None, PASSWORD = None, BOTFIRSTNAME = None, BOTSURNAME = None, HOST = 'wss://api.test.mucklet.com', ORIGIN = 'https://test.mucklet.com', VERBOSE = False):
+        # create the logging classes.
+        self.BootingLog = Logger("Booting Info")
+        self.MessageLogger = Logger("Recieved Message")
+        self.SendingLogger = Logger("Sending Message")
+        self.ErrorLogger = Logger("Error")
+        self.characterLogger = Logger("Character Info")
         # work out if the user is using a token or a username and password.
         if TOKEN:
             self.TOKEN = TOKEN
@@ -102,6 +177,7 @@ class Bot:
         self.bootIndex = 0
         # create the players hash if the player type is a player
         if self.BOTTYPE == "player":
+            self.BootingLog.log("Creating the players hash.")
             self.pepper = "TheStoryStartsHere"
             m = hashlib.sha256()
             m.update(bytes(self.PASSWORD,'UTF-8'))
@@ -115,8 +191,7 @@ class Bot:
         msg = json.dumps(request)
         ws.send(msg)
         if self.VERBOSE:
-            print("sent: ",msg)
-            print("--------")
+            self.SendingLogger.log(f"{msg}")
 
     def on_message(self, ws, message):
         #Message handler- basically everything taps here
@@ -124,18 +199,17 @@ class Bot:
 
         #Be loud?
         if self.VERBOSE:
-            print("recieved: ",msg)
-            print("--------")
+            self.MessageLogger.log(f"{msg}")
 
         #Error message handler- currently just to pass boot if already controlled or awake
         if 'error' in msg and self.bootIndex < 6:
-            if self.bootIndex == 3 and msg['id'] == CTRL_BOT:
-                self.bootIndex = 4
-            if self.bootIndex == 4 and msg['id'] == WAKEUP:
+            if self.bootIndex == 4 and msg['id'] == CTRL_BOT:
                 self.bootIndex = 5
+            if self.bootIndex == 5 and msg['id'] == WAKEUP:
+                self.bootIndex = 6
         elif 'error' in msg:
-            #Prints errors outside boot cycle
-            print(msg)
+            #log errors outside boot cycle
+            self.ErrorLogger.log(f"{msg}")
 
         #For reply responses
         if 'result' in msg and self.bootIndex < 7:
@@ -151,16 +225,18 @@ class Bot:
                 self.bootIndex = 3
             if self.bootIndex == 3 and msg['id'] == GET_CHARS:
                 if self.BOTTYPE == "player":
+                    characters = []
                     #Grab the char data to search to ID the bot
                     chars = msg["result"]["collections"][self.RID+".chars"]
                     chars = [a['rid'] for a in chars] #Get the actual refs
                     for ch in chars:
                         #Loop over the chars
                         char_data = msg["result"]["models"][ch]
-                        print("    " + char_data["name"] + " " + char_data["surname"] + " : " + char_data['id'])
+                        characters.append(char_data["name"] + " " + char_data["surname"] + " : " + char_data['id'])
                         if char_data["name"] == self.BOTFIRSTNAME and char_data["surname"] == self.BOTSURNAME:
                             #If find the bot's name, get its ID and make the char object
                             self.CID = char_data["id"]
+                    self.characterLogger.log(*characters)
                 else:
                     pass
                 self.bootIndex = 4
@@ -181,26 +257,23 @@ class Bot:
             if self.OnMessage != None:
                 self.OnMessage(self, Message(self, msg))
             else:
-                print("No OnMessage function set.")
-                print("It Would Recieve: ", msg)
+                if self.VERBOSE:
+                    self.ErrorLogger.log("No OnMessage function defined.")
         
 
     def on_error(self, ws, error):
         #Helper function to wrap WS function
-        print("error: ",error)
-        print("--------")
+        self.ErrorLogger.log(f"{error}")
 
-    def on_close(self, ws):
+    def on_close(self, ws, close_status_code = None, close_msg = None):
         #Helper function to wrap WS function
         if self.VERBOSE:
-            print("### closed ###")
-            print("--------")
+            self.BootingLog.log('connection closed')
 
     def on_open(self, ws):
         #Helper function to wrap WS function
         if self.VERBOSE:
-            print("### opened ###")
-            print("--------")
+            self.BootingLog.log('connection opened')
         self.bootIndex = 1
         self.ws = ws
         # thread the boot cycle
@@ -223,7 +296,7 @@ class Bot:
 
         #Boot sequence started in separate thread, waits until on_open is called in WS
         #In thread to have messages, so it's run in order to build the char objects cleanly
-        print("AUTHENTICATING")
+        self.BootingLog.log("Authenticating")
         #Authenticate and get the player info
         if self.BOTTYPE == "bot":
             params = {"token":self.TOKEN}
@@ -235,7 +308,7 @@ class Bot:
         while (self.bootIndex == 1):
             time.sleep(0.1)
 
-        print("GETTING PLAYER RID")
+        self.BootingLog.log("Getting Player RID")
         #Get Player rid
         if self.BOTTYPE == "bot":
             self.send(self.ws,'call.core.getBot',ind=GET_PLAYER)
@@ -244,43 +317,44 @@ class Bot:
         while(self.bootIndex == 2):
             time.sleep(0.1)
 
-        print("GETTING CHARS SET")
+        self.BootingLog.log("Getting Character Data")
         #Get character data
         self.send(self.ws,"get."+self.RID,ind=GET_CHARS)
         while(self.bootIndex == 3):
             time.sleep(0.1)
 
-        print("CONTROLLING BOT")
+        self.BootingLog.log("Controlling Bot")
         #Take control of the bot character
         method = "call."+self.RID+".controlChar"
         self.send(self.ws,method,params={'charId':self.CID},ind=CTRL_BOT)
         while(self.bootIndex == 4):
             time.sleep(0.1)
 
-        print("WAKING BOT")
+        self.BootingLog.log("Waking Bot")
         #Wake the bot up
         method = "call.core.char."+self.CID+".ctrl.wakeup"
         self.send(self.ws,method,ind=WAKEUP)
         while(self.bootIndex == 5):
             time.sleep(0.1)
 
-        print("GETTING CHAR DATA")
+        self.BootingLog.log("Subscribing to all incoming data")
         #Get looking character data
         self.send(self.ws,"subscribe."+self.RID,ind=GET_CHARS)
         while(self.bootIndex == 6):
             time.sleep(0.1)
 
         #Done booting
-        print("BOOTED")
+        self.BootingLog.log("Done booting")
 
         #Call user's OnStart function if it exists in a new thread
         if self.OnStart != None:
             onStartThread = threading.Thread(target=self.OnStart, args=(self,))
             onStartThread.start()
         else:
-            print("No OnStart function set.")
+            if self.VERBOSE:
+                self.ErrorLogger.log("No OnStart function defined.")
 
-    def gosleep(self):
+    def sleep(self):
         method = "call.core.char."+self.CID+".ctrl.sleep"
         self.send(self.ws,method,params={},ind=SLEEP)
 
@@ -315,3 +389,11 @@ class Bot:
     def ping(self):
         method = "call.core.char."+self.CID+".ctrl.ping"
         self.send(self.ws,method,params={},ind=PING)
+    
+    def getID(self, name):
+        method = "call."+self.RID+".getChar"
+        self.send(self.ws, method, params={"charName": name})
+    
+    def quit(self):
+        self.sleep()
+        self.ws.close()
